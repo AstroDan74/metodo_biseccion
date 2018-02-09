81c578fb04d601675861b2dddbcfef94e7e2e1b3
meatdan <daniel74op@gmail.com>
2018-02-08 21:20:01 -0600

    Programa escrito en clase
    Copia del programa que hice en clase con un comentario

---
+++ ceros.f90
@@ -0,0 +1,19 @@
+program ceros
+  ! hey hey
+  a=-4
+  b=4
+  n=1000
+  h=(b-a)/n
+  do i=0,n
+    xi=a+(h*i)
+    xi1=a+(h*(i+1))
+    fi=f(xi)
+    fi1=f(xi1)
+    mult=fi*fi1
+    if (mult.gt.0) print*,'Hay una raíz entre',xi,'y',xi1,''
+  enddo
+end
+
+function f(x)
+  f= -19.*(x-.5)*(x-1.)+exp(x)-exp(-2.*x)
+end function
